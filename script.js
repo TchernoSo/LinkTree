16124a1f41de0e686f1fc8b23db1c0d48b17d850
Tcherno So <tcherno.so@hotmail.com>
2023-12-07 23:45:32 +0000

    Form done

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-const message = "Welcome to MySpace";
-const speed = 150;
-
-var i = 0;
-
-function typeEffect(){
-    document.getElementById("text").innerHTML += message.charAt(i);
-    i++;
-
-    setTimeout(typeEffect, speed);
-}
-
-typeEffect();
-
-
-
-$('button').click(function(){
-    $('.success').addClass('show');
-    $('.success').addClass('alert');
-    $('.success').removeClass('hide');
-    setTimeout(function(){
-        $('.success').removeClass('show');
-        $('.success').addClass('hide');
-        
-    })
-});
-
+function message(){
+    var Name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var msg = document.getElementById('msg');
+    const success = document.getElementById('success');
+    const danger = document.getElementById('danger');
+  
+    if(Name.value === '' || email.value === '' || msg.value === ''){
+        danger.style.display = 'block';
+    }
+    else{
+        setTimeout(() => {
+            Name.value = '';
+            email.value = '';
+            msg.value = '';
+        }, 2000);
+  
+        success.style.display = 'block';
+    }
+  
+  
+    setTimeout(() => {
+        danger.style.display = 'none';
+        success.style.display = 'none';
+    }, 4000);
+  
+  }
+  
+  
+  const text = "Welcome to MySpace";
+  const speed = 150;
+  
+  var i = 0;
+  
+  function typeEffect(){
+      document.getElementById("text").innerHTML += text.charAt(i);
+      i++;
+  
+      setTimeout(typeEffect, speed);
+  }
+  
+  typeEffect();
+  
+  
